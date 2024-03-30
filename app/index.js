a7ce1a0159c3ca04cfcc5a855dcabd472b94e6fe
Deiiivy <deivypr28@gmail.com>
2024-03-30 18:07:08 -0500

    add logic key equal userKey

--- app/index.js
+++ app/index.js
@@ -11,10 +11,15 @@ btnTheme.addEventListener('click', () => {
 
 const textContainer = document.querySelector('.text');
 const restarText = document.querySelector('.btnReset')
+const successKeyE = document.querySelector('#succesKey')
+const incorrretKeyE = document.querySelector('#incorretKey')
+
+let successKey = 0;
+let incorrretKey = 0;
 
 function textRandom(){
     const texts = [
-        'Hola, ¿cómo estás?',
+        'Hola, ¿cómo estas?',
         'Bienvenido al juego',
         'El perro ladra',
         'La casa es grande',
@@ -22,24 +27,52 @@ function textRandom(){
         'Estamos aprendiendo mucho',
         'El tiempo vuela',
         'La vida es bella',
-        'Mañana será otro día',
-        'Todo saldrá bien'
+        'Mañana será otro dia',
+        'Todo saldra bien'
     ];
 
     let textRandomIndex = Math.floor(Math.random()*texts.length)
     let textRandom = texts[textRandomIndex]
 
 textContainer.innerHTML = textRandom
+
+return textRandom
+
 }
-textRandom()
 
 
+let currentText = textRandom()
+
+successKeyE.innerHTML = successKey
+incorrretKeyE.innerHTML = incorrretKey
+
+document.addEventListener('keydown',(e) => {
+    console.log('event ' + e.key);
+    calKey(e.key)
+})
+
+function calKey(keyUser) {
+    if (currentText[successKey] === keyUser) {
+        successKey++;
+    } else {
+        incorrretKey++;
+    }
+    successKeyE.innerHTML = successKey
+incorrretKeyE.innerHTML = incorrretKey
+
+    
+}
 
 
 function resetGame() {
     restarText.addEventListener('click', () => {
-        textRandom()
+        currentText = textRandom(); 
+        successKey = 0;
+        incorrretKey = 0;
+        successKeyE.innerHTML = successKey;
+        incorrretKeyE.innerHTML = incorrretKey; 
     })
 }
 
 resetGame()
+
