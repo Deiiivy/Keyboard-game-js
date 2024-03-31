1688ac9639ed262e4f1841bb2878e93dfcc38298
Deiiivy <deivypr28@gmail.com>
2024-03-30 21:56:19 -0500

    add btnRestar and text Random

--- app/index.js
+++ app/index.js
@@ -7,3 +7,39 @@ btnTheme.addEventListener('click', () => {
 })
 
 
+// Config Game
+
+const textContainer = document.querySelector('.text');
+const restarText = document.querySelector('.btnReset')
+
+function textRandom(){
+    const texts = [
+        'Hola, ¿cómo estás?',
+        'Bienvenido al juego',
+        'El perro ladra',
+        'La casa es grande',
+        'Me gusta el chocolate',
+        'Estamos aprendiendo mucho',
+        'El tiempo vuela',
+        'La vida es bella',
+        'Mañana será otro día',
+        'Todo saldrá bien'
+    ];
+
+    let textRandomIndex = Math.floor(Math.random()*texts.length)
+    let textRandom = texts[textRandomIndex]
+
+textContainer.innerHTML = textRandom
+}
+textRandom()
+
+
+
+
+function resetGame() {
+    restarText.addEventListener('click', () => {
+        textRandom()
+    })
+}
+
+resetGame()
